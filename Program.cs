3f5c21a96efa3118036ce34d5dcedde39d4c62e2
joaovictoralvesmartins90 <jvam90@proton.me>
2024-04-28 17:11:37 -0300

    Adicionando injeção de dependência para repositório de atores.

--- Program.cs
+++ Program.cs
@@ -22,6 +22,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IGenresRepository, GenresRepository>();
+builder.Services.AddScoped<IActorsRepository, ActorsRepository>();
 builder.Services.AddAutoMapper(typeof(Program));
 //Serviços
 
