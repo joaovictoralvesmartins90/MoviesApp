22c78cb16fcab7f081ec0a3a2985210cfc2eb7a3
joaovictoralvesmartins90 <jvam90@proton.me>
2024-04-28 17:03:47 -0300

    Criando repositório de atores.

---
+++ Repositories/ActorsRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesApp.DatabaseContext;
+using MoviesApp.Entities;
+
+namespace MoviesApp.Repositories
+{
+    public class ActorsRepository : IActorsRepository
+    {
+        private readonly ApplicationDbContext context;
+
+        public ActorsRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<int> Create(Actor actor)
+        {
+            context.Add(actor);
+            await context.SaveChangesAsync();
+            return actor.Id;
+        }
+
+        public async Task Delete(int id)
+        {
+            await context.Actors.Where(x => x.Id == id).ExecuteDeleteAsync();
+        }
+
+        public async Task<bool> Exists(int id)
+        {
+            return await context.Actors.AnyAsync(x => x.Id == id);
+        }
+
+        public async Task<List<Actor>> GetAll()
+        {
+            return await context.Actors.OrderBy(x => x.Name).ToListAsync();
+        }
+
+        public Task<Actor?> GetById(int id)
+        {
+            return context.Actors.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task Update(Actor actor)
+        {
+            context.Actors.Update(actor);
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Repositories/IActorsRepository.cs
@@ -0,0 +1,14 @@
+﻿using MoviesApp.Entities;
+
+namespace MoviesApp.Repositories
+{
+    public interface IActorsRepository
+    {
+        Task<int> Create(Actor actor);
+        Task<List<Actor>> GetAll();
+        Task<Actor?> GetById(int id);
+        Task<bool> Exists(int id);
+        Task Update(Actor actor);
+        Task Delete(int id);
+    }
+}
